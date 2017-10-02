3cdaffbc280c08cd5039dedd99badea27675e322
NgocB <ngoc.bach@eastagile.com>
2017-10-02 16:13:05 +0700

    Update all gem and fix seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 # Init Dictionary
-Dictionary.create!(name: 'Oxford Dictionaries', hompage: 'https://www.oxforddictionaries.com/',
+Dictionary.create!(name: 'Oxford Dictionaries', homepage: 'https://www.oxforddictionaries.com/',
   api_homepage: 'https://developer.oxforddictionaries.com/')
-Dictionary.create!(name: 'Words API', hompage: 'https://www.wordsapi.com/',
+Dictionary.create!(name: 'Words API', homepage: 'https://www.wordsapi.com/',
   api_homepage: 'https://www.wordsapi.com/')
